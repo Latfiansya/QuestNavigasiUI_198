7433eb619c6f826a58d2b48ab14c353ff59f45b4
Latfiansya Dzikri - 198 <latfiansyadzikri@gmail.com>
2025-11-07 13:06:45 +0700

    Mengatur dasar tampilan (scaffold) untuk tampilData.kt

--- app/src/main/java/com/example/activity5/view/tampilData.kt
+++ app/src/main/java/com/example/activity5/view/tampilData.kt
@@ -35,5 +35,14 @@ fun TampilData(
         Pair(stringResource(id = R.string.jenis_kelamin), "Lainnya"),
         Pair(stringResource(id = R.string.alamat), "Yogyakarta")
     )
-
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text = stringResource(id = R.string.tampil), color = Color.White) },
+                colors = TopAppBarDefaults.mediumTopAppBarColors(
+                    containerColor = colorResource(id = R.color.teal_700)
+                )
+            )
+        }
+    )
 }
