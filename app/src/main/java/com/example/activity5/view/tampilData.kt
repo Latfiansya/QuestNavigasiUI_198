f54a9bfa23733175e43c9212667e0ec752a33782
Latfiansya Dzikri - 198 <latfiansyadzikri@gmail.com>
2025-11-07 13:11:11 +0700

    Membuat btn Back

--- app/src/main/java/com/example/activity5/view/tampilData.kt
+++ app/src/main/java/com/example/activity5/view/tampilData.kt
@@ -74,6 +74,12 @@ fun TampilData(
 
             Spacer(modifier = Modifier.height(10.dp))
 
+            Button(
+                modifier = Modifier.fillMaxWidth(),
+                onClick = onBackBtnClick
+            ) {
+                Text(text = stringResource(id = R.string.back))
+            }
         }
     }
 }
