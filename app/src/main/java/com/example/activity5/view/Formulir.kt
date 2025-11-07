295d42e8c98ecf9cc585d47de3ddf0af8ff80992
Latfiansya Dzikri - 198 <latfiansyadzikri@gmail.com>
2025-11-07 09:55:24 +0700

    Membuat fun FormIsian

--- app/src/main/java/com/example/activity5/view/Formulir.kt
+++ app/src/main/java/com/example/activity5/view/Formulir.kt
@@ -1,2 +1,17 @@
 package com.example.activity5.view
 
+import android.R
+import androidx.annotation.experimental.Experimental
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+
+@OptIn(markerClass = ExperimentalMaterial3Api::class)
+@Composable
+fun FormIsian(
+    jenisK:List<R.string> = listOf("Laki-laki", "Perempuan"),
+    OnSubmitBtnClick : () -> Unit
+){
